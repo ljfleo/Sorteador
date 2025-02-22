cffeacbaa506e81391854b98c3846718075ea262
Leonardo Ferrari <leoferrari.jw@gmail.com>
2025-02-22 17:00:23 -0300

    algoritmo otimizado versão final

--- app.js
+++ app.js
@@ -1,46 +1,37 @@
 function gerarNumeroAleatorio(max, min) {
-    // essa função gera os números aleatórios de um intervalo x a y
-    return parseInt(Math.random() * (max - min) + min + 1);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function sortearNumeros() {
-    // essa é a função principal, ela irá sortear os números que o usuário escolher
     let numeroEscolhido;
-    let resultado = [];
-    let comecoSorteio = 0;
+    let sorteados = [];
     let numeroInicial = parseInt(document.getElementById('de').value);
     let numeroFinal = parseInt(document.getElementById('ate').value);
     let quantidadeDeNumeros = document.getElementById('quantidade').value;
     let quantidadeDePossibilidades = numeroFinal - numeroInicial;
 
-    while (comecoSorteio < quantidadeDeNumeros) {
+    for (let i = 0; i < quantidadeDeNumeros; i++) {
         if (quantidadeDePossibilidades < quantidadeDeNumeros) {
             alterarMensagem('resultado', 'A quantidade escolhida é maior que as possibilidades');
             break;
         } else {
             numeroEscolhido = gerarNumeroAleatorio(numeroFinal, numeroInicial);
-            while (resultado.includes(numeroEscolhido)) {
+            while (sorteados.includes(numeroEscolhido)) {
                 numeroEscolhido = gerarNumeroAleatorio(numeroFinal, numeroInicial);
             }
-            resultado.push(numeroEscolhido);
-
-            comecoSorteio++;
+            sorteados.push(numeroEscolhido);
         }
-        alterarMensagem('resultado', `Os números sorteados são : ${resultado}`);
+        alterarMensagem('resultado', `Os números sorteados são : ${sorteados}`);
     }
-
-
     document.getElementById('btn-reiniciar').className = 'container__botao';
 }
 
 function alterarMensagem(id, texto) {
-    // essa função será utilizada para alterar as mensagens no front-end da aplicação
     let campo = document.getElementById(id);
     campo.innerHTML = texto;
 }
 
 function reiniciarJogo() {
-    // essa função irá reiniciar o jogo do início
     document.getElementById('btn-reiniciar').className = 'container__botao-desabilitado';
     limparCampo('resultado');
     limparCampo('de');
