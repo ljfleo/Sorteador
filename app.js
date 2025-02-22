81a7c65b8c0b3b3dd0c8606ff2b98e49d30d5d04
Leonardo Ferrari <leoferrari.jw@gmail.com>
2025-02-22 07:35:32 -0300

    Versão Final

--- app.js
+++ app.js
@@ -5,16 +5,31 @@ function gerarNumeroAleatorio(max, min) {
 
 function sortearNumeros() {
     // essa é a função principal, ela irá sortear os números que o usuário escolher
+    let numeroEscolhido;
     let resultado = [];
     let comecoSorteio = 0;
     let numeroInicial = parseInt(document.getElementById('de').value);
     let numeroFinal = parseInt(document.getElementById('ate').value);
     let quantidadeDeNumeros = document.getElementById('quantidade').value;
+    let quantidadeDePossibilidades = numeroFinal - numeroInicial;
+
     while (comecoSorteio < quantidadeDeNumeros) {
-        resultado.push(gerarNumeroAleatorio(numeroFinal, numeroInicial))
-        comecoSorteio++;
+        if (quantidadeDePossibilidades < quantidadeDeNumeros) {
+            alterarMensagem('resultado', 'A quantidade escolhida é maior que as possibilidades');
+            break;
+        } else {
+            numeroEscolhido = gerarNumeroAleatorio(numeroFinal, numeroInicial);
+            while (resultado.includes(numeroEscolhido)) {
+                numeroEscolhido = gerarNumeroAleatorio(numeroFinal, numeroInicial);
+            }
+            resultado.push(numeroEscolhido);
+
+            comecoSorteio++;
+        }
+        alterarMensagem('resultado', `Os números sorteados são : ${resultado}`);
     }
-    alterarMensagem('resultado', `Os números sorteados são : ${resultado}`);
+
+
     document.getElementById('btn-reiniciar').className = 'container__botao';
 }
 
